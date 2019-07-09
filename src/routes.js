a73b461ce8a24403c4d2b940324684b4804a3ec5
Jean Lobo <jeanloboxd@gmail.com>
2019-07-09 13:22:17 -0300

    chore: Criado Model User, Controller UserController e Rota para a criação do User

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,19 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.post('/users', UserController.store);
+
+// routes.get('/', async (req, res) => {
+//   const user = await User.create({
+//     name: 'Raiza Alves',
+//     email: 'raiza1_jp@hotmail.com',
+//     password_hash: '8979347239847',
+//   });
+
+//   return res.json(user);
+// });
 
 export default routes;
