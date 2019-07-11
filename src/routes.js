8c029731d8bf935f3a775c71ac346c14f3b0a5ee
Jean Lobo <jeanloboxd@gmail.com>
2019-07-11 13:30:08 -0300

    chore: Criado tratamento no evento antes de salvar do model user para criptografar senha para o usuário

--- src/routes.js
+++ src/routes.js
@@ -6,14 +6,4 @@ const routes = new Router();
 
 routes.post('/users', UserController.store);
 
-// routes.get('/', async (req, res) => {
-//   const user = await User.create({
-//     name: 'Raiza Alves',
-//     email: 'raiza1_jp@hotmail.com',
-//     password_hash: '8979347239847',
-//   });
-
-//   return res.json(user);
-// });
-
 export default routes;
