c619c98b9712fdcc82251cc51af3e20b6975fb0f
Jean Lobo <jeanlobocd@gmail.com>
2019-07-22 22:22:01 -0300

    chore: Add ScheduleController.js

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -3,8 +3,37 @@ import { startOfHour, parseISO, isBefore } from 'date-fns';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
+import File from '../models/File';
 
 class AppointmentControler {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      order: ['date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.json(appointments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -15,7 +44,6 @@ class AppointmentControler {
       return res.status(400).json({ error: 'Validation fails' });
     }
     const { provider_id, date } = req.body;
-
     /**
      * Check if provider_id is a provider
      */
@@ -28,7 +56,6 @@ class AppointmentControler {
         .status(401)
         .json({ error: 'You can only create appointments with providers' });
     }
-
     /**
      * Check for past dates
      */
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
@@ -20,6 +21,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
