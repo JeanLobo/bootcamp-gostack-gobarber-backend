94d61eaf6b23990b895a842543cf82d6b2331da3
Jean Lobo <jeanlobocd@gmail.com>
2019-07-25 22:33:01 -0300

    chore: Add Notifications and Mails

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '2a43174255622e',
+    pass: 'be2f69e8305690',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
